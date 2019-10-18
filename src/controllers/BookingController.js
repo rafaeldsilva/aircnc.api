2219cf7f7c9b9c24311ec761020d41c0ff872bb7
rafaeldsilva <rafael.dsilva@outlook.com.br>
2019-10-17 23:57:58 -0300

    Alteração no spotModel

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -4,6 +4,7 @@ const User = require('../models/User');
 
 module.exports = {
     async store(req, res){
+        
         const { user_id } = req.headers;
         const { spot_id } = req.params;
         const { date } = req.body;
--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 
 module.exports = {
    async store(req, res) {
+       
         const { email } = req.body;
 
         let user = await User.findOne({ email });
