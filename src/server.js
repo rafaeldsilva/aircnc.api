10eb1869080e854ba3276c941a449f55ca7bc0b3
rafaeldsilva <rafael.dsilva@outlook.com.br>
2019-10-18 19:07:23 -0300

    make it better

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-
+const Port = process.env.PORT || 3333
 const app = express();
 
 mongoose.connect('mongodb+srv://usr_aircnc_db:aircnc2k19@cluster0-hlave.mongodb.net/test?retryWrites=true&w=majority',{
@@ -16,5 +16,5 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+app.listen(Port);
  
